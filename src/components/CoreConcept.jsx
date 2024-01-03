fbecffa01150ea5e41af343cdf9cf9faca26633c
someshshenoy5 <shenoysomesh5@gmail.com>
2024-01-03 16:19:17 +0530

    basics of react component

---
+++ src/components/CoreConcept.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+
+export default function CoreConcept({image,title,description}){
+  return (
+    <div>
+    <li>
+    <img src={image} alt={title} />
+    <h3>{title}</h3>
+    <p>{description}</p>
+    </li>
+    </div>
+    
+  );
+}
+
+
---
+++ src/components/TabButton.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function TabButton({ children,onSelect,isSelected }) {
+  
+  return (
+    <li>
+      <button className={isSelected ? 'active' : ''} onClick={onSelect}>{children}</button>
+    </li>
+  );
+}
